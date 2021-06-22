501699d34baaf815f09ef3db8b2682884b72ff09
Brady Clark <bradyjclark88@gmail.com>
2021-06-22 13:43:57 +0800

    Emojifi chars. Whitespace missing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 struct Sentence {
     sentence: String
 }
@@ -5,5 +7,15 @@ fn main() {
     let arg = std::env::args().nth(1).expect("No sentence supplied");
     let pattern = String::from(arg);
     let sentence = Sentence { sentence: pattern };
-    println!("{}", sentence.sentence);
+    // println!("{}", sentence_to_emoji(sentence.sentence));
+    sentence_to_emoji(sentence.sentence);
+}
+
+// fn sentence_to_emoji(sentence: String) -> String {
+fn sentence_to_emoji(sentence: String) {
+    let re = Regex::new(r"(?P<character>[[:alpha:]])").unwrap();
+    for cap in re.captures_iter(&sentence) {
+        let emojified = format!(":alphabet-yellow-{}:", &cap["character"]);
+        print!("{}", emojified)
+    }
 }
